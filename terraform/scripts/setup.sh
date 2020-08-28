4c6fcc4b6b657922c8888a5127293ab68e3d9fc4
markr1966 <markrafferty27@gmail.com>
2020-08-28 09:31:26 +0000

    added script to install Maven and Apache Maven on Master VM

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -49,4 +49,17 @@ chmod +x ./kubectl
 # move binary to correct location
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+# Install Maven
+sudo apt install maven
+
+# Install Apache Maven
+wget https://www-us.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz -P /tmp
+sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
+sudo ln -s /opt/apache-maven-3.6.0 /opt/maven
+export JAVA_HOME=/usr/lib/jvm/default-java
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
+
+
 sudo reboot
